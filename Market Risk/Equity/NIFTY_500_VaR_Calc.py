b46eb8b0064164e007fff5a71d0018d9f3c6c8d9
fobic <sabychakraborty08@gmail.com>
2024-07-01 17:04:33 +0530

    feature (Market Risk -> Equity) : added VaR calcs for more Equity Assets.

---
+++ Market Risk/Equity/NIFTY_500_VaR_Calc.py
@@ -0,0 +1,20 @@
+# The data is from 1995 to 2024
+
+import numpy as np
+import pandas as pd
+
+file_path = 'farse 1.0/Market Risk/Equity/data/NIFTY 500_Historical_PR_01011990to01072024.csv'
+nifty_500_data = pd.read_csv(file_path, parse_dates=['Date'])
+
+nifty_500_data.set_index('Date', inplace=True)
+
+print(nifty_500_data.head())
+
+nifty_500_data['Log Return'] = np.log(nifty_500_data['Close'] / nifty_500_data['Close'].shift(1))
+
+nifty_500_data.dropna(inplace=True)
+
+confidence_level = 0.95
+
+VaR = np.percentile(nifty_500_data['Log Return'], (1 - confidence_level) * 100)
+print(f"Value at Risk (VaR) at {confidence_level*100}% confidence level: {VaR}")
