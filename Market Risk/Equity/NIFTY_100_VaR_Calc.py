173310e9c3c0a4dcd646b4778a908810165d3f8a
fobic <sabychakraborty08@gmail.com>
2024-07-01 16:31:06 +0530

    function(Market Risk -> Equity) : made a model which calculates the VaR on Equity Assets primarily NIFTY for the past 34 Years (1990 to 2024).

---
+++ Market Risk/Equity/NIFTY_100_VaR_Calc.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+file_path = 'farse 1.0/Market Risk/Equity/data/NIFTY 100_Historical_PR_01022006to01072024.csv'
+nifty_100_data = pd.read_csv(file_path, parse_dates=['Date'])
+
+nifty_100_data.set_index('Date', inplace=True)
+
+print(nifty_100_data.head())
+
+nifty_100_data['Log Return'] = np.log(nifty_100_data['Close'] / nifty_100_data['Close'].shift(1))
+
+nifty_100_data.dropna(inplace=True)
+
+confidence_level = 0.95
+
+VaR = np.percentile(nifty_100_data['Log Return'], (1 - confidence_level) * 100)
+print(f"Value at Risk (VaR) at {confidence_level*100}% confidence level: {VaR}")
+
---
+++ Market Risk/Equity/NIFTY_50_VaR_Calc.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+file_path = 'farse 1.0/Market Risk/Equity/data/NIFTY 50_Historical_PR_01011990to01072024.csv'
+nifty_50_data = pd.read_csv(file_path, parse_dates=['Date'])
+
+nifty_50_data.set_index('Date', inplace=True)
+
+print(nifty_50_data.head())
+
+nifty_50_data['Log Return'] = np.log(nifty_50_data['Close'] / nifty_50_data['Close'].shift(1))
+
+nifty_50_data.dropna(inplace=True)
+
+confidence_level = 0.95
+
+VaR = np.percentile(nifty_50_data['Log Return'], (1 - confidence_level) * 100)
+print(f"Value at Risk (VaR) at {confidence_level*100}% confidence level: {VaR}")
+
